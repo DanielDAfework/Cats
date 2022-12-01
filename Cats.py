253af9331f0e5dbca32fd8e717985e2309024c19
Daniel Afework <danielafework@Daniels-MacBook-Air.local>
2022-11-30 23:50:01 -0800

    Initial commit

---
+++ Cats.py
@@ -0,0 +1,27 @@
+from twilio.rest import Client
+import requests
+import os
+from boto.s3.connection import S3Connection
+import schedule 
+import time
+
+def ShipCat():
+    catImage = requests.get('https://api.thecatapi.com/v1/images/search').json()[0]['url']
+
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        to=os.environ['TO'],
+        from_=os.environ['FROM'],
+        body="Free Cat :)",
+        media_url=catImage,
+    )
+
+schedule.every(60).minutes.do(ShipCat)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
